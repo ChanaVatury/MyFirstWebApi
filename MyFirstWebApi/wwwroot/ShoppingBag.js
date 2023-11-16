35d356cdc814eda9cf99ae1d66289cbe0782b44a
ChanaVatury <cv3285191@gmail.com>
2023-11-16 17:11:21 +0200

    hg

--- MyFirstWebApi/wwwroot/ShoppingBag.js
+++ MyFirstWebApi/wwwroot/ShoppingBag.js
@@ -1,15 +1,13 @@
 
 let count = sessionStorage.getItem("count");
 async function drawProducts() {
-    for (let i = 0; i < count; i++) {
-        draw(sessionStorage.getItem("MyCart")[i+2])
+    let prod = JSON.parse(draw(sessionStorage.getItem("MyCart")));
+    for (let i = 0; i < prod.length; i++) {
+        var tmpCatg = document.getElementById("temp-card");
+        var cln = tmpCatg.content.cloneNode(true);
+        cln.querySelector(".price").innerText = prod.price;
+        cln.querySelector("imageColumn").src = "./images/" + prod.image;
+        document.getElementById("PoductList").appendChild(cln);
     }
 }
 
-function draw(prod) {
-    var tmpCatg = document.getElementById("temp-card");
-    var cln = tmpCatg.content.cloneNode(true);
-    cln.querySelector(".price").innerText = prod.price;
-    cln.querySelector("imageColumn").src = "./images/" + prod.image;
-    document.getElementById("PoductList").appendChild(cln);
-}
