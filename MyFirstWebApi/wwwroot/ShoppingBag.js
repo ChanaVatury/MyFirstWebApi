2645f36e562a0a62371419897d8a05c6bb675f72
ChanaVatury <cv3285191@gmail.com>
2023-11-20 21:28:08 +0200

    '

--- MyFirstWebApi/wwwroot/ShoppingBag.js
+++ MyFirstWebApi/wwwroot/ShoppingBag.js
@@ -9,21 +9,14 @@ async function drawProducts() {
         var cln = tmpCatg.content.cloneNode(true);
         cln.querySelector(".price").innerText = prod[i].price;
         cln.querySelector("img").src = "./images/" + prod[i].image;
-        cln.querySelector(".totalColumn").addEventListener('click', () => { deleteProd(prod[i]) });
+        cln.querySelector(".totalColumn").addEventListener('click', () => {
+            prod.splice(i, 1);
+            sessionStorage.setItem("MyCart", prod);
+            drawProducts();
+        });
         document.getElementById("items").appendChild(cln);
     }
 }
 
-async function deleteProd(pro) {
-    let p = prod;
-    prod = [];
-    for (let i = 0; i < p.length; i++) {
-        if (p[i].id == p.id) {
-            prod.push(p[i])
-        }
-    }
-    sessionStorage.setItem("MyCart", p);
-    let prod = JSON.parse(sessionStorage.getItem("MyCart"));
-    drawProducts();
-}
+ 
 
--- Repository/ShoppingBookContext.cs
+++ Repository/ShoppingBookContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Entities;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Repository;
 
@@ -29,7 +30,8 @@ public partial class ShoppingBookContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Server=srv2\\pupils;Database=ShoppingBook;Trusted_Connection=True;TrustServerCertificate=True");
-
+    //
+    //"Server=DESKTOP - E0FAPSB\\SQLEXPRESS;Database=ShoppingBook;Trusted_Connection=True;TrustServerCertificate=True"
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Category>(entity =>
