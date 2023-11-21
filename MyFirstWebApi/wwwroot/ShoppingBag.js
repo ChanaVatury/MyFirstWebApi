091f8ac9d71ed6026e8cc6298dc97d07bec5adf7
ChanaVatury <cv3285191@gmail.com>
2023-11-21 13:48:24 +0200

    rr

--- MyFirstWebApi/wwwroot/ShoppingBag.js
+++ MyFirstWebApi/wwwroot/ShoppingBag.js
@@ -1,21 +1,27 @@
 
 let count = sessionStorage.getItem("count");
 let prod = JSON.parse(sessionStorage.getItem("MyCart"));
-async function drawProducts() {
- 
+//let count2 = prod.length;
+
+async function drawProducts() { 
+    document.getElementById("itemCount").innerHTML = count;
     for (let i = 0; i < prod.length; i++) {
         console.log(prod[i])
         var tmpCatg = document.getElementById("temp-row");
         var cln = tmpCatg.content.cloneNode(true);
         cln.querySelector(".price").innerText = prod[i].price;
         cln.querySelector("img").src = "./images/" + prod[i].image;
-        cln.querySelector(".totalColumn").addEventListener('click', () => {
-            prod.splice(i, 1);
-            sessionStorage.setItem("MyCart", prod);
-            drawProducts();
-        });
+        cln.querySelector(".totalColumn").addEventListener('click', () => deletFromCart(prod[i]));
         document.getElementById("items").appendChild(cln);
     }
+}
+
+async function deletFromCart(prod1) {
+    prod = prod.filter(c => c != prod1);
+    sessionStorage.removeItem("MyCart");
+    sessionStorage.setItem("MyCart", JSON.stringify(prod));
+    document.getElementById("items").replaceChildren([]);
+    drawProducts();
 }
 
  
